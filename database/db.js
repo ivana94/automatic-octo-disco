76730d7abb61c865b03a3ec80ecac1a79466f792
ivana94 <ivmatij@gmail.com>
2020-06-15 17:45:40 +0200

    project setup

---
+++ database/db.js
@@ -0,0 +1,8 @@
+const spicedPg = require("spiced-pg");
+let db = spicedPg(
+  process.env.DATABASE_URL ||
+    "postgres:postgres:postgres@localhost:5432/betches-hp"
+);
+
+exports.getUserById = id => db.query("SELECT * FROM users WHERE id = $1", [id]);
+exports.getAllUserNames = id => db.query("SELECT first FROM users");
